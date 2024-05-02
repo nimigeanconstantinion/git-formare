327cf885fec2b4a937bc8bd708b65f5f56254a2b
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-05-02 18:55:27 +0300

    test 003

--- ci/build-publish.sh
+++ ci/build-publish.sh
@@ -6,18 +6,18 @@
 : "${TAG:?TAG not set or empty}"
 
 # Create a new builder instance and use it
-docker buildx create --use
-
-# Build and push the Docker image
-
-docker buildx build \
-    --platform=linux/amd64,linux/arm64 \
-    -t "${USERNAME}/${REPO}:${TAG}" \
-    -t "${USERNAME}/${REPO}:latest" \
-    "${@:2}" \
-    --push \
-    "$1"
-    # Ensure this points to the directory with Dockerfile
-
-
-#docker push "${USERNAME}/${REPO}:${TAG}"
+#docker buildx create --use
+#
+## Build and push the Docker image
+#
+#docker buildx build \
+#    --platform=linux/amd64,linux/arm64 \
+#    -t "${USERNAME}/${REPO}:${TAG}" \
+#    -t "${USERNAME}/${REPO}:latest" \
+#    "${@:2}" \
+#    --push \
+#    "$1"
+#    # Ensure this points to the directory with Dockerfile
+#
+docker tag "${USERNAME}/${REPO}" "${USERNAME}/${REPO}:${TAG}"
+docker push "${USERNAME}/${REPO}:${TAG}"

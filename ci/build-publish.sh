f4758f10e8beb58d0c1fa8d37d07199c293e8d73
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-05-02 19:38:07 +0300

    test 010

--- ci/build-publish.sh
+++ ci/build-publish.sh
@@ -6,13 +6,16 @@
 : "${TAG:?TAG not set or empty}"
 : "${APPNAME:?APPNAME not set or empty}"
     echo "ok: USERNAME, APPNAME, și TAG SUNT SETATE"
-
+if ! docker images | grep -q "${APPNAME}"; then
+    echo "Imaginea locală ${APPNAME} nu a fost găsită."
+    exit 1
+fi
 
 # Create a new builder instance and use it
 #docker buildx create --use
+##
+## Build and push the Docker image
 #
-# Build and push the Docker image
-
 #docker buildx build \
 #    --platform=linux/amd64,linux/arm64 \
 #    -t "${USERNAME}/${APPNAME}:${TAG}" \
@@ -21,5 +24,5 @@
 #    --push \
 #    "$1"
 #    # Ensure this points to the directory with Dockerfile
-
-docker tag formare-api "${USERNAME}/${APPNAME}:${TAG}"
+#
+##docker tag formare-api "${USERNAME}/${APPNAME}:${TAG}"
